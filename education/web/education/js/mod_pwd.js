5fac53b34d72ac3a2aaaa80204a0382edbf396e9
cloudni <cloudni@users.noreply.github.com>
2016-05-13 16:23:57 +0800

    --add force logout after update password

--- education/web/education/js/mod_pwd.js
+++ education/web/education/js/mod_pwd.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
     function showResponse(responseText, statusText)  {
         if(responseText.s == 1){
            alert("重置密码成功");
+            window.location.href = "/index.php?r=/education/admin/logout";
         }else{
             $("#errormsg").html(responseText.data).show(300).delay(3000).hide(300); 
         }
